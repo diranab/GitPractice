c2c47d2bd5d554df9a491bf4bd4b469d11b2732f
Akin Abiola <loluab52@gmail.com>
2020-05-07 20:58:19 -0500

    create a text file to test

--- main.cpp
+++ main.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
-
+#include <vector>
+#include <utility>
+#include <fstream>
 
 class readCSV
 {
 
 };
+
+//READ FUNCTION
+//reads csv file and returns a vector of pair<column name, vector of all elements in the column>
+//column name is a string type and the vector of elements in the column is of integer type
+
+std::vector<std::pair<std::string, std::vector<int>>> read_csv(std::string filepath) 
+{
+	
+}
+
 
 int main()
 {
